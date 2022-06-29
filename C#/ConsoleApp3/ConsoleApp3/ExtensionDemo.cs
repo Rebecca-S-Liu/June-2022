be8dcda7d51a719f8ce1d6b574a32d20a40e0067
Rebecca-S-Liu <87039929+Rebecca-S-Liu@users.noreply.github.com>
2022-06-29 14:52:43 -0400

    Create ConsoleApp3

---
+++ C#/ConsoleApp3/ConsoleApp3/ExtensionDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public static class ExtensionDemo
+    {
+        public static string EvenOrOdd(this int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            } else
+            {
+                return "Odd";
+            }
+        }
+    }
+}
---
+++ C#/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp3;
+using ConsoleApp3.Presentation;
+
+//int a = 10;
+//Console.WriteLine($"The number {a} is {a.EvenOrOdd()}");
+
+//Console.WriteLine(GenericsDemo.AreEqual(12,23));
+//Console.WriteLine(GenericsDemo.AreEqual("segoudf", 23.3324));
+
+//Console.WriteLine(GenericsDemo<string>.AreEqual("hello","world"));
+//Console.WriteLine(GenericsDemo<double>.AreEqual(3.23,2342));
+
+ManageCustomer manageCustomer = new ManageCustomer();
+manageCustomer.Run();
